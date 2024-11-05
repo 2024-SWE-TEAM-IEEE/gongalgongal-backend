5ed32148d5689115fe79be57abd740d6de54fee7
euneun17 <0617ye@gmail.com>
2024-11-05 14:50:27 +0900

    [CHORE] #1 Add initial Spring project

---
+++ src/test/java/gongalgongal/gongalgongal_spring/GongalgongalSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package gongalgongal.gongalgongal_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GongalgongalSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
