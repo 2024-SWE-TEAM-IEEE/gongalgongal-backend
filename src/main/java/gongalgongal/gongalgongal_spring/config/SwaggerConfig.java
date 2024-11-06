36d6cc74fff0cb6c8e0d055cfe8d176a277fb6f2
euneun17 <0617ye@gmail.com>
2024-11-06 21:39:52 +0900

    [CHORE] Swagger 연동

---
+++ src/main/java/gongalgongal/gongalgongal_spring/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package gongalgongal.gongalgongal_spring.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// Swagger URL => http://localhost:8080/swagger-ui/index.html#/
+@Configuration    // 스프링 실행시 설정파일 읽어드리기 위한 어노테이션
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Team IEEE's Swagger")
+                .description("GongalGongal REST API")
+                .version("1.0.0");
+    }
+}
