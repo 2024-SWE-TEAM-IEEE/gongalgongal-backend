38d2b4841e1caf89e82bd0e44ea5c9766a0511a9
Suchae Jeong <zmfhtmgjsej@dgu.ac.kr>
2024-11-10 14:29:40 +0900

    [FIX] User.java 오류 수정
    한글로 적은 설명이 주석 없이 작성돼있었음. 제거함.

--- src/main/java/gongalgongal/gongalgongal_spring/model/User.java
+++ src/main/java/gongalgongal/gongalgongal_spring/model/User.java
@@ -7,13 +7,6 @@ import jakarta.persistence.Id;
 import lombok.Data;
 
 
-네, Repository 인터페이스는 기본 키 타입이 Long에서 BIGINT로 변경되어도 그대로 유지됩니다. JPA에서 기본 키 타입이 Long인 엔티티는 MySQL의 BIGINT와 호환되기 때문에, Repository는 변경 없이 그대로 사용해도 됩니다.
-
-변경된 SQL 스키마에 맞춰 각 엔티티 클래스를 조정한 코드는 다음과 같습니다.
-
-1. User 엔티티
-java
-코드 복사
 @Entity
 @Data
 public class User {
